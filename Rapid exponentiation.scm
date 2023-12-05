8e669b1cd47b29208f4fbd828b3c725ec74a81c4
mila454 <88385276+mila454@users.noreply.github.com>
2023-12-05 14:35:10 +0300

    Update and rename Rapid exponentiation to Rapid exponentiation.scm

--- Rapid exponentiation.scm
+++ Rapid exponentiation.scm
@@ -1,3 +1,4 @@
+;;V1
 (define (solution b n)
   (cond ((= n 0) 1)
         ((even? n) (squire (solution b (/ n 2))))
@@ -12,3 +13,14 @@
 (define (reminder n m)
   (- n (* m (floor (/ n m)))))
 (check-equal? (solution 10 0) 1)
+
+;;V2
+(define (solution b n)
+  (expt-iter b n 1))
+
+(define (expt-iter b counter product)
+  (if (= counter 0)
+      product
+      (expt-iter b
+                (- counter 1)
+                (* b product))))
